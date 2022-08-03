233a48d589bd383964ad4306133b5eeb658a90e7
Atziri <211241028@alumnos.utn.edu.mx>
2022-08-03 03:20:10 -0500

    d

--- src/main/java/com/LeonardoLopez/Org/Repositorio/VacantesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LeonardoLopez.Org.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.LeonardoLopez.Org.Model.Vacante;
-
-public interface VacantesRepository extends JpaRepository <Vacante,Integer>{
-
-}
